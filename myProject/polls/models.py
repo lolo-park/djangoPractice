1e6bab33add318f8a778af8684b01dc7f2555c6e
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-04-05 13:33:59 +0900

    Creating Models

--- myProject/polls/models.py
+++ myProject/polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# 각 모델은 :class:`django.db.models.Model`의 하위 클래스로 표현된다
+# 모델마다 여러 클래스 변수가 있으며 각 클래스 변수는 모델에서 데이터베이스 필드를 나타낸다
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
