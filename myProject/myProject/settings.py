3352ab59f5d93a846116c03fb56138d1a3e5495c
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-04-29 23:34:53 +0900

    @api_view 데코레이터 적용

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0$u5ykt-g&btgi18z$gi*w5=e8x)_*+xn0^eaqxw9b&vcoo*ti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -90,11 +90,14 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- myProject/snippets/urls.py
+++ myProject/snippets/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+# url 패턴에 suffix를 추가하여 요청된 포맷에 따라 적절한 응답을 반환하는 것을 지원
+# `/api/snippets.json` or `/api/snippets/.xml` 같은 url 요청이 들언다면
+# view 에서는 `request` 객체의 `format` 속성을 사용하여 요청된 포맷을 확인하고 알맞은 응답을 반환할 수 있음
 
